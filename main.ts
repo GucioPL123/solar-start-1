4a0a54a44dc0daeaa8719acab458dbd0c26b3157
GucioPL123 <166158045+GucioPL123@users.noreply.github.com>
2024-11-03 19:29:46 +0100

    Początkowe pliki dla projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,30 @@
+input.onButtonPressed(Button.A, function () {
+    ONoff = 1
+    basic.showNumber(1)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    ONoff = 0
+    basic.showNumber(0)
+    basic.pause(500)
+    basic.clearScreen()
+})
+let ONoff = 0
+ONoff = 0
+radio.setGroup(1)
+basic.forever(function () {
+    if (ONoff == 1 && input.lightLevel() > 75) {
+        radio.sendNumber(1)
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            `)
+        basic.clearScreen()
+    } else {
+    	
+    }
+})
