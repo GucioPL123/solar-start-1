8f61aaf4ccc2f24520bf0f8d6f7f68980dcfaf4d
SzpiegDonPedro <166158045+SzpiegDonPedro@users.noreply.github.com>
2024-11-05 17:47:46 +0100

    radio reset

--- main.ts
+++ main.ts
@@ -1,19 +1,63 @@
+enum RadioMessage {
+    message1 = 49434
+}
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(1)
-    basic.pause(400)
-    ONoff = true
-    music.play(music.tonePlayable(587, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
-    basic.pause(500)
+    radio.setGroup(2)
+    radio.sendMessage(RadioMessage.message1)
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        . . # . .
+        . . . # .
+        . . . . #
+        `)
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        . . # . .
+        . . . # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
     basic.clearScreen()
 })
-input.onButtonPressed(Button.B, function () {
-    ONoff = false
-    basic.showNumber(0)
-    music.play(music.tonePlayable(330, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
-    basic.pause(500)
-    basic.clearScreen()
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (ONoff == false) {
+        ONoff = true
+        basic.showNumber(1)
+        music.play(music.tonePlayable(587, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+        basic.pause(500)
+        basic.clearScreen()
+    } else if (ONoff == true) {
+        ONoff = false
+        basic.showNumber(0)
+        music.play(music.tonePlayable(330, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+        basic.pause(500)
+        basic.clearScreen()
+    }
 })
 let ONoff = false
+radio.setTransmitPower(7)
 ONoff = false
 radio.setGroup(1)
 loops.everyInterval(50, function () {
