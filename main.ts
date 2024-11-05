72a3043a37cdfbc487345f467659273758cf9322
SzpiegDonPedro <166158045+SzpiegDonPedro@users.noreply.github.com>
2024-11-05 16:07:47 +0100

    Pełna wersja

--- main.ts
+++ main.ts
@@ -2,19 +2,21 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(1)
     basic.pause(400)
     ONoff = true
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
     basic.pause(500)
     basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
     ONoff = false
     basic.showNumber(0)
+    music.play(music.tonePlayable(330, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
     basic.pause(500)
     basic.clearScreen()
 })
 let ONoff = false
 ONoff = false
 radio.setGroup(1)
-loops.everyInterval(10, function () {
+loops.everyInterval(50, function () {
     if (ONoff == true && input.lightLevel() < 205) {
         radio.sendNumber(1)
         basic.showLeds(`
@@ -26,18 +28,5 @@ loops.everyInterval(10, function () {
             `)
         basic.pause(3000)
         basic.clearScreen()
-    } else if (ONoff == true) {
-        basic.showLeds(`
-            # . . . .
-            # # . . .
-            # # # . .
-            # # # # .
-            # # # # #
-            `)
-        basic.pause(2000)
-        basic.clearScreen()
     }
-})
-basic.forever(function () {
-	
 })
